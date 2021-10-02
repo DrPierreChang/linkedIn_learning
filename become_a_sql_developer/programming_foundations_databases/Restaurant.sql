74b1d554a2e7b8dd61e99525d81353301ba056e3
DrPierreChang <aaa.lavrentev@gmail.com>
2021-10-02 21:24:54 +1000

    basic queries

--- become_a_sql_developer/programming_foundations_databases/Restaurant.sql
+++ become_a_sql_developer/programming_foundations_databases/Restaurant.sql
@@ -19,18 +19,30 @@ SELECT Name FROM Dishes;
 
 SELECT FirstName, LastName, Email FROM Customers;
 
-SELECT FirstName, LastName, Email, State FROM Customers WHERE State="CA" OR State="CO";
+SELECT FirstName, LastName, Email, State FROM Customers WHERE State='CA' OR State='CO';
 
 -- first symbol is C, second is any
-SELECT FirstName, LastName, Email, State FROM Customers WHERE State LIKE "C%";
+SELECT FirstName, LastName, Email, State FROM Customers WHERE State LIKE 'C%';
 
-SELECT CustomerID, LastName, Email, State FROM Customers WHERE FirstName="Taylor";
+SELECT CustomerID, LastName, Email, State FROM Customers WHERE FirstName='Taylor';
 
 SELECT * FROM Reservations;
 
-SELECT * FROM Reservations WHERE Date > "2019-02-06" AND Date < "2019-02-07";
+SELECT * FROM Reservations WHERE Date > '2019-02-06' AND Date < '2019-02-07';
 
 -- sorting in alphabetical order by name. To sort in reverse order use DESC
 SELECT * FROM Dishes ORDER BY Name;
 SELECT * FROM Dishes ORDER BY Name DESC;
+SELECT * FROM Dishes ORDER BY Price DESC;
+SELECT * FROM Reservations ORDER BY Date DESC;
 
+SELECT * FROM Reservations
+WHERE Date > '2019-02-06' AND Date < '2019-02-07'
+ORDER BY Date DESC;
+
+SELECT COUNT(FirstName) FROM Customers;
+
+SELECT  COUNT(FirstName) FROM Customers
+WHERE State='CA';
+
+SELECT SUM(Price), AVG(Price), MIN(Price), MAX(Price) FROM Dishes;
